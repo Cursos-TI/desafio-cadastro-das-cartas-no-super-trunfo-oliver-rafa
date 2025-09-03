672ef9d81ac5252d171ce4e16088a1baa960e54a
oliver-rafa <olivermitora@gmail.com>
2025-09-03 00:12:11 +0000

    supertrunfocompleto

--- Super_Trunfo/CartasSuperTrunfo.c
+++ Super_Trunfo/CartasSuperTrunfo.c
@@ -80,7 +80,7 @@ int main() {
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
@@ -92,7 +92,7 @@ int main() {
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
